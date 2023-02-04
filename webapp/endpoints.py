d0949ab79d45ee9a3da254415311aeeba844ff8a
Nati <netanelzelzermn@gmail.com>
2023-02-05 00:02:10 +0200

    Added templates

--- webapp/endpoints.py
+++ webapp/endpoints.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter
-from fastapi import Depends
+from fastapi import APIRouter, Depends, FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from dependency_injector.wiring import inject, Provide
 from .containers import Container
 from .services import ShoppingCartService
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-
+templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/test")
-def test():
-    return {"data": "testtttttttttttttttttttt"}
+def test(request: Request):
+    return templates.TemplateResponse("cart.html", {"request": request, 'id': 'Test'})
 
 @router.get("/cart/user/{user_id}")
 @inject
@@ -18,6 +20,15 @@ def get_cart_by_user_id(user_id: int,
                    cart_service: ShoppingCartService =
                    Depends(Provide[Container.cart_service])):
     return cart_service.get_cart_by_user_id(user_id)
+
+@router.get("/cart/user/{user_id}/html", response_class=HTMLResponse)
+@inject
+def get_cart_by_user_id_html(request: Request, user_id: int,
+                   cart_service: ShoppingCartService =
+                   Depends(Provide[Container.cart_service])):
+    data = cart_service.get_cart_by_user_id(user_id)
+
+    return templates.TemplateResponse("cart.html", {"request": request, "user_id": user_id, "data": data})
 
 @router.delete("/cart/user/{user_id}")
 @inject
